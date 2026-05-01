714d33431824241a07df876a8711f746d354e2fa
deren3rcan <derenercan04@gmail.com>
2026-05-01 22:42:38 +0300

    Update Planlama.java

--- src/model/Planlama.java
+++ src/model/Planlama.java
@@ -5,6 +5,7 @@ public class Planlama {
     private int id;
     private int siparisId;
     private String siparisKodu;
+    private String urunAdi;
     private String gorev;
     private String makine;
     private String planTarihi;
@@ -18,42 +19,24 @@ public class Planlama {
         this.durum = "Bekliyor";
     }
 
-    public Planlama(int id, int siparisId, String siparisKodu, String gorev,
-                    String makine, String planTarihi, String durum) {
+    public Planlama(int id, int siparisId, String siparisKodu, String urunAdi,
+                    String gorev, String makine, String planTarihi, String durum) {
         this.id = id;
         this.siparisId = siparisId;
         this.siparisKodu = siparisKodu;
+        this.urunAdi = urunAdi;
         this.gorev = gorev;
         this.makine = makine;
         this.planTarihi = planTarihi;
         this.durum = durum;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public int getSiparisId() {
-        return siparisId;
-    }
-
-    public String getSiparisKodu() {
-        return siparisKodu;
-    }
-
-    public String getGorev() {
-        return gorev;
-    }
-
-    public String getMakine() {
-        return makine;
-    }
-
-    public String getPlanTarihi() {
-        return planTarihi;
-    }
-
-    public String getDurum() {
-        return durum;
-    }
+    public int getId() { return id; }
+    public int getSiparisId() { return siparisId; }
+    public String getSiparisKodu() { return siparisKodu; }
+    public String getUrunAdi() { return urunAdi; }
+    public String getGorev() { return gorev; }
+    public String getMakine() { return makine; }
+    public String getPlanTarihi() { return planTarihi; }
+    public String getDurum() { return durum; }
 }
